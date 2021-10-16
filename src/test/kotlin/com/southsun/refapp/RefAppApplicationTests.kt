858c9aeeab8f77fadf0b852f5651c52d0b8f6a65
Marcin Zioło <martin.ziolo@gmail.com>
2021-10-16 16:32:33 +0200

    initial commit

---
+++ src/test/kotlin/com/southsun/refapp/RefAppApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.southsun.refapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RefAppApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
