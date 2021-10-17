446493bb1f579cdcc3b9b1246d3531746e8412b7
Marcin Zioło <martin.ziolo@gmail.com>
2021-10-17 21:12:26 +0200

    Removing manully added sources

--- src/main/kotlin/com/southsun/refapp/models/ErrorResponse.kt
+++
@@ -1,5 +0,0 @@
-//package com.southsun.refapp.models
-//
-//data class ErrorResponse(
-//    private val message: String
-//)
--- src/main/kotlin/com/southsun/refapp/models/Request.kt
+++
@@ -1,7 +0,0 @@
-//package com.southsun.refapp.models
-//
-//data class Request(
-//    private val customerId: String,
-//    private val month: String?,
-//    private val year: String
-//)
