0008d6b3cac8ee2ef65180321d7a05f3338f0b04
Marcin Zioło <martin.ziolo@gmail.com>
2021-10-17 21:10:28 +0200

    Working example

--- src/main/kotlin/com/southsun/refapp/controller/HelloWorldController.kt
+++ src/main/kotlin/com/southsun/refapp/controller/HelloWorldController.kt
@@ -1,8 +1,6 @@
 package com.southsun.refapp.controller
 
-import com.southsun.refapp.models.Account
 import com.southsun.refapp.models.CustomerDetails
-import io.swagger.models.Response
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
